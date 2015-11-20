e8e5241474c2fb7a8fc4d4a242f98665b2a1067a
jjordantb <jordan.florchinger@uleth.ca>
2015-11-19 20:03:10 -0700

    setup game tests format

--- tests/TestGame.cpp
+++ tests/TestGame.cpp
@@ -3,13 +3,29 @@
 CPPUNIT_TEST_SUITE_REGISTRATION(TestGame);
 
 void TestGame::setUp() {
-
+  game = new Chess();
 }
 
 void TestGame::tearDown() {
-
+  delete game;
 }
 
 void TestGame::testConstructor() {
-  
+
+}
+
+void TestGame::testSetup() {
+
+}
+
+void TestGame::testIsOver() {
+
+}
+
+void TestGame::testTurn() {
+
+}
+
+void TestGame::testPlay() {
+
 }
--- tests/TestGame.h
+++ tests/TestGame.h
@@ -4,17 +4,31 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 #include "../Game.h"
+#include "../Chess.h"
 
 class TestGame : public CppUnit::TestFixture {
 
   CPPUNIT_TEST_SUITE(TestGame);
   CPPUNIT_TEST(testConstructor);
+  CPPUNIT_TEST(testSetup);
+  CPPUNIT_TEST(testIsOver);
+  CPPUNIT_TEST(testTurn);
+  CPPUNIT_TEST(testPlay);
   CPPUNIT_TEST_SUITE_END();
+
+private:
+  Game* game;
 
 public:
   void setUp();
   void tearDown();
+
   void testConstructor();
+
+  void testSetup();
+  void testIsOver();
+  void testTurn();
+  void testPlay();
 
 };
 
